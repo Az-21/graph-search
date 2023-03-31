0b40c7df12f3b20a4cf0118963de6b67c50bbdd3
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-04-01 02:15:08 +0530

    Add debug printer for the parsed square matrix

---
+++ Track/Progress.cs
@@ -0,0 +1,25 @@
+﻿namespace Search.Track;
+public static class Progress
+{
+  public static void ParsedSquareMatrix(double[,] matrix)
+  {
+    // Get dimension of square matrix
+    int n = matrix.GetLength(0);
+
+    // Header
+    string header = $"Parsed CSV as a(n) {n}x{n} matrix";
+    string underline = new('-', header.Length);
+    Console.WriteLine($"{header}\n{underline}\n");
+
+    // NxN matrix
+    for (int i = 0; i < n; i++)
+    {
+      for (int j = 0; j < n; j++)
+      {
+        Console.Write(matrix[i, j].ToString("00.0"));
+        Console.Write("  ");
+      }
+      Console.WriteLine("\n");
+    }
+  }
+}
