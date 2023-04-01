ed7d964245ba2f7b2c0530292b1dc5a9a57074e5
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-04-01 14:44:59 +0530

    Add an intermediate layer to handle search calls

---
+++ SearchMethod/Handler.cs
@@ -0,0 +1,16 @@
+﻿namespace Search.SearchMethod;
+public enum UsingMethod { DepthFirstSearch }
+
+public static class Handler
+{
+  public static void SearchWith(in double[][] matrix, in int startNode, in int goalNode, in UsingMethod usingMethod)
+  {
+    switch (usingMethod)
+    {
+      case UsingMethod.DepthFirstSearch:
+        SearchMethod.DepthFirstSearch.Run(in matrix, in startNode, goalNode); break;
+
+      default: throw new Exception("Unrecognized UsingMethod enum passed");
+    }
+  }
+}
