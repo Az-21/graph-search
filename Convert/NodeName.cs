e963a496299438586a683f8f20daa2c2a7c97112
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-04-01 10:59:26 +0530

    Add utility function to convert number to alphabet and vice versa

---
+++ Convert/NodeName.cs
@@ -0,0 +1,34 @@
+﻿namespace Search.Convert;
+public static class NodeName
+{
+  // Base10 to Base26 | 0->A, 1->B, 25->Z, 26->AA
+  public static string FromNumberToAlphabet(int number)
+  {
+    string result = string.Empty;
+
+    while (number >= 0)
+    {
+      int remainder = number % 26;
+      result = (char)(65 + remainder) + result;
+      number = (number / 26) - 1;
+    }
+
+    return result;
+  }
+
+  // Base26 to Base10 | A->0, B->1, Z->25, AA->26
+  public static int FromAlphabetToNumber(in string alphabet)
+  {
+    int result = 0;
+    int power = 0;
+
+    foreach (char c in alphabet)
+    {
+      int value = c - 'A';
+      result += (int)Math.Pow(26, power) * (value + 1);
+      power++;
+    }
+
+    return result - 1;
+  }
+}
