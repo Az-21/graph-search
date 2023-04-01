576a415f9e8d1169eb0c44a78b8a77175386f38a
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-04-02 00:33:13 +0530

    Improve output formatting
    - Added spacer
    - Added headers

--- SearchMethod/Handler.cs
+++ SearchMethod/Handler.cs
@@ -11,10 +11,20 @@ public static class Handler
     // Print parsed matrix | IF_DEF
     Print.Matrix.FromParsedCsv(in matrix);
 
-    // Uninformed Search -> i) Depth first search (DFS)
+    // DFS
+    Spacer();
+    Print.Header.WithUnderline("Uninformed Search -> i) Depth first search (DFS)");
     SearchMethod.DepthFirstSearch.Run(in matrix, in startNode, in goalNode);
 
-    // Uninformed Search -> ii) Breadth first search (BFS)
+    // BFS
+    Spacer();
+    Print.Header.WithUnderline("Uninformed Search -> ii) Breadth first search (BFS)");
     SearchMethod.BreadthFirstSearch.Run(in matrix, in startNode, in goalNode);
+  }
+
+  private static void Spacer()
+  {
+    string divider = new('/', 80);
+    Console.WriteLine($"\n\n{divider}\n{divider}\n\n");
   }
 }
