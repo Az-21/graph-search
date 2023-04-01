6b48c5c173cca229a5cb32ad64cfee7e70a7dad1
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-04-01 11:28:30 +0530

    Add `readonly` modifier to schema

--- Network/Schema.cs
+++ Network/Schema.cs
@@ -4,15 +4,15 @@ public static class Schema
   // Child of node -> name and movement cost
   public readonly record struct Child
   {
-    public int Name { get; init; }
-    public double Cost { get; init; }
+    public readonly int Name { get; init; }
+    public readonly double Cost { get; init; }
   }
 
   // Node -> depth, h(n), and array of Child
   public readonly record struct Node
   {
-    public int Depth { get; init; }
-    public double Heuristic { get; init; }
-    public Child[] Children { get; init; }
+    public readonly int Depth { get; init; }
+    public readonly double Heuristic { get; init; }
+    public readonly Child[] Children { get; init; }
   }
 }
