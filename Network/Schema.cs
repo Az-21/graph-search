838c27dc7f671719f8df539846abd85b804ae1a6
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-04-01 11:10:41 +0530

    Add node schema `struct`

---
+++ Network/Schema.cs
@@ -0,0 +1,18 @@
+﻿namespace Search.Network;
+public static class Schema
+{
+  // Child of node -> name and movement cost
+  public readonly record struct Child
+  {
+    public int Name { get; init; }
+    public double Cost { get; init; }
+  }
+
+  // Node -> depth, h(n), and array of Child
+  public readonly record struct Node
+  {
+    public int Depth { get; init; }
+    public double Heuristic { get; init; }
+    public Child[] Children { get; init; }
+  }
+}
