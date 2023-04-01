8987a2319080e3e545ee9ee8fa4e8919589d42e9
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-04-01 14:45:42 +0530

    Update program to use `SearchMethod.Handler` and `IF_DEF`

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
-﻿namespace Search;
+﻿using Search.SearchMethod;
 
+namespace Search;
 internal static class Program
 {
-  const bool DEBUG = true;
+  // Global constants for debugging and tracing steps
+  public const bool DEBUG = true;
+  public const bool USE_ALPHABET = true;
+
   static void Main()
   {
     // Read and parse adjacency matrix from CSV
-    double[,] matrix = Input.Matrix.ParseSquareMatrixCsv("graph01.csv");
-    if (DEBUG) { Track.Progress.ParsedSquareMatrix(in matrix); }
+    double[][] matrix = Input.Matrix.ParseSquareMatrixCsv("graph01.csv");
+
+    // Run search
+    const UsingMethod method = SearchMethod.UsingMethod.DepthFirstSearch;
+    SearchMethod.Handler.SearchWith(in matrix, 0, 6, method);
   }
 }
