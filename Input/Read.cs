cbb189a240577f4844796515862b412eb8ff9f1d
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-04-02 19:41:29 +0530

    Hide `IF_DEF` flags behind a wrapper
    To ignore 'unreachable' code warnings

---
+++ Input/Read.cs
@@ -0,0 +1,7 @@
+﻿namespace Search.Input;
+// This is to hide the 'unreachable code' warning. Not the best practice, but *it is what it is*.
+public static class Read
+{
+  public static bool DebugFlag() => Program.DEBUG;
+  public static bool AlphabetFlag() => Program.USE_ALPHABET;
+}
