26fd5e7d102dfe929d9f7702e53756da2542f3be
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-04-02 20:49:57 +0530

    Update search handler to use config struct and add DLS call

--- SearchMethod/Handler.cs
+++ SearchMethod/Handler.cs
@@ -3,23 +3,23 @@ public static class Handler
 {
   public static void SearchUsingAllMethods(in double[][] matrix, in Program.SearchWith searchWith)
   {
-    // Unpack search options
-    int startNode = searchWith.StartNode;
-    int goalNode = searchWith.GoalNode;
-    int depthLimit = searchWith.DepthLimit;
-
     // Print parsed matrix | IF_DEF
     Print.Matrix.FromParsedCsv(in matrix);
 
     // DFS
     Spacer();
     Print.Header.WithUnderline("Uninformed Search -> i) Depth first search (DFS)");
-    SearchMethod.DepthFirstSearch.Run(in matrix, in startNode, in goalNode);
+    DepthFirstSearch.Run(in matrix, in searchWith);
 
     // BFS
     Spacer();
     Print.Header.WithUnderline("Uninformed Search -> ii) Breadth first search (BFS)");
-    SearchMethod.BreadthFirstSearch.Run(in matrix, in startNode, in goalNode);
+    BreadthFirstSearch.Run(in matrix, in searchWith);
+
+    // DLS
+    Spacer();
+    Print.Header.WithUnderline("Uninformed Search -> iii) Depth limited search (DLS)");
+    DepthLimitedSearch.Run(in matrix, in searchWith);
   }
 
   private static void Spacer()
