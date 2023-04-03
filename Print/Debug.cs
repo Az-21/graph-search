1fc017f8f4379b4071a5e0a1cd8b03cfc2906627
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-04-03 11:55:43 +0530

    Add debug messages for UCS

--- Print/Debug.cs
+++ Print/Debug.cs
@@ -9,7 +9,10 @@ public enum About
   NoUniqueChild,
   AddedToVisited,
   AlreadyVisited,
+  AppendedWithCost,
   ReachedDepthLimit,
+  PoppedNodeWithMinCuCost,
+  CostHigherThanCurrentBest,
 };
 
 public static class Debug
@@ -42,6 +45,30 @@ public static class Debug
 
       case Print.About.ReachedDepthLimit:
         Console.WriteLine($"\tx `Node {id}` is at depth limit [d={depth}] -> not expanding further\n");
+        break;
+    }
+  }
+
+  // Debug messages for search methods which use filtering based on cumulative cost
+  public static void Message(in int node, in About message, in double cost, in double best = 0)
+  {
+    if (!Input.Read.DebugFlag()) { return; }
+    string id = Convert.NodeName.ConvertToNumberOrAlphabet(node);
+
+    switch (message)
+    {
+      case Print.About.AppendedWithCost:
+        Console.WriteLine($"\t+ Appended `Node {id}` with cumulative cost of {cost}");
+        Console.WriteLine($"\t  Previous best was {best}");
+        break;
+
+      case Print.About.CostHigherThanCurrentBest:
+        Console.WriteLine($"\tx Not appending `Node {id}` with cumulative cost of {cost}");
+        Console.WriteLine($"\t  Current best is {best}");
+        break;
+
+      case Print.About.PoppedNodeWithMinCuCost:
+        Console.WriteLine($"Popped `Node {id}` from the stack/queue because it has lowest cumulative sum of {cost}");
         break;
     }
   }
