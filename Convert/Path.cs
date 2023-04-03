4d6296fc28cec10ed4ca095d2f7480e48ffce9e1
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-04-03 21:56:35 +0530

    Add logic to convert path to cost of path

---
+++ Convert/Path.cs
@@ -0,0 +1,17 @@
+﻿namespace Search.Convert;
+public static class Path
+{
+  public static double ToCost(in double[][] matrix, in List<int> path)
+  {
+    double sum = 0;
+    for (int i = 0; i < path.Count - 1; i++)
+    {
+      int origin = path[i];
+      int destination = path[i + 1];
+
+      sum += matrix[origin][destination];
+    }
+
+    return sum;
+  }
+}
