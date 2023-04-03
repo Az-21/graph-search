74b2427b643ac251d9f2c113832664b4c4f457cd
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-04-03 22:12:45 +0530

    Add debug message for path cost calculation

--- Convert/Path.cs
+++ Convert/Path.cs
@@ -4,12 +4,16 @@ public static class Path
   public static double ToCost(in double[][] matrix, in List<int> path)
   {
     double sum = 0;
-    for (int i = 0; i < path.Count - 1; i++)
+    int nodes = path.Count;
+
+    for (int i = 0; i < nodes - 1; i++)
     {
       int origin = path[i];
       int destination = path[i + 1];
-
       sum += matrix[origin][destination];
+
+      Print.Path.TravelCost(in matrix, in origin, in destination);
+      if (Input.Read.DebugFlag() && i != nodes - 2) { Console.Write(" + "); }
     }
 
     return sum;
--- Print/Path.cs
+++ Print/Path.cs
@@ -12,4 +12,14 @@ public static class Path
     List<string> fPath = Convert.NodeName.ConvertListToNumberOrAlphabet(in path);
     Console.WriteLine(string.Join(separator, fPath));
   }
+
+  // Debug message for cost involved with traveling from one node to another
+  public static void TravelCost(in double[][] matrix, in int origin, in int destination)
+  {
+    if (!Input.Read.DebugFlag()) { return; }
+
+    string fOrigin = Convert.NodeName.ConvertToNumberOrAlphabet(origin);
+    string fDestination = Convert.NodeName.ConvertToNumberOrAlphabet(destination);
+    Console.Write($"/ {fOrigin} >> {fDestination} = {matrix[origin][destination]} /");
+  }
 }
