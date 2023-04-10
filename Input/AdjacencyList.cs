a1c76de7ac49fb24bfba658930ef38da1631b116
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-04-10 11:01:51 +0530

    Add function to convert adjacency list (JSON) to matrix (double[][])

---
+++ Input/AdjacencyList.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json;
+
+namespace Search.Input;
+public static class AdjacencyList
+{
+  public static double[][] ConvertToMatrix(string json)
+  {
+    // Preprocess JSON
+    json = json.Trim();
+    json = json.ToUpperInvariant();
+
+    // De-serialize the adjacency list JSON
+    Dictionary<string, string>? adjList = JsonSerializer.Deserialize<Dictionary<string, string>>(json);
+    if (adjList is null)
+    {
+      Console.WriteLine("[ FATAL ]\tCannot parse adjacency matrix. Ensure JSON is formatted correctly.");
+      System.Environment.Exit(2);
+    }
+
+    // Even isolated and one-way nodes need to be present for this to work.
+    List<string> nodes = adjList.Keys.ToList();
+    int size = nodes.Count;
+
+    // Generate NxN matrix -> fill M(i,j) with 1 if an edge exists
+    double[][] matrix = new double[size][];
+    for (int i = 0; i < size; i++)
+    {
+      matrix[i] = new double[size];
+      string[] edges = adjList[nodes[i]].Split(",", StringSplitOptions.TrimEntries);
+      foreach (string edge in edges)
+      {
+        int j = nodes.IndexOf(edge);
+        matrix[i][j] = 1;
+      }
+    }
+
+    return matrix;
+  }
+}
